854631bb85703305fd59b53e9d081bc8ba1d1864
Cassidy James Blaede <c@ssidyjam.es>
2016-06-12 16:49:36 -0600

    Minor cleanup

--- elementary.js
+++ elementary.js
@@ -1,12 +1,13 @@
 function elementary(ua) {
-    var e = "elementary OS",
+    var e = "elementary OS/0.4",
         m = "Midori/0.5",
-        u = "Linux";
+        l = "Linux";
 
     if ( ua.indexOf(e) > -1 ) {
         return "0.4";
-    } else if ( (ua.indexOf(m) >-1) && (ua.indexOf(u) >-1) ) {
-        return "0.3";
+    } else if ( (ua.indexOf(m) >-1) && (ua.indexOf(l) >-1) ) {
+        // Running Midori 0.5 on Linux, so very possibly Freya.
+        return "0.3"; // FIXME: incorrectly reports 0.3 for Midori in Loki
     }
 
     return false;
